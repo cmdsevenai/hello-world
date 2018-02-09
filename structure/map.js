808a246c4e3ca27da7bc0d457dd299099f141079
Seven Eleven <ylinylin777@gmail.com>
2018-02-09 10:07:55 +0100

    adjust

--- structure/map.js
+++ structure/map.js
@@ -1,15 +1,14 @@
 $(function(){ 
-
 	window.map = {};
 	map = new ol.Map({
 		target: 'map',
 		controls: [],
 		layers: grouplayers,
-        view: new ol.View({
-          center: ol.proj.transform([11.63, 52.12], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 6
-        })
-    });	
+        	view: new ol.View({
+          		center: ol.proj.transform([11.63, 52.12], 'EPSG:4326', 'EPSG:3857'),
+          		zoom: 6
+        	})
+    	});	
 	
 	//### add controls
 	var overview = new ol.control.OverviewMap();
